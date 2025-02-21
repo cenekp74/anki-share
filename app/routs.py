d86c0baeb648c42c9d4a8d287b8e921501361e56
Čeněk Použník <potuznikcenek@gmail.com>
2025-02-21 21:30:45 +0100

    filepond upload backend

--- app/routs.py
+++ app/routs.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import flash, render_template, redirect, url_for, jsonify, abort, request, send_file, send_from_directory
 from werkzeug.utils import secure_filename
+from app.utils import random_hex_token
+import os
 
 @app.route('/favicon.ico')
 def send_favicon():
@@ -11,6 +13,19 @@ def send_favicon():
 def index():
     return render_template("index.html")
 
-@app.route('/upload')
+@app.route('/upload', methods=["POST", "GET"])
 def upload():
-    return render_template("upload.html")
+    if request.method == "GET":
+        return render_template("upload.html")
+    
+    file = request.files.get('filepond')
+    filename = secure_filename(file.filename)
+    if not filename.endswith(".apkg"):
+        abort(400)
+    token = random_hex_token()
+    
+    os.mkdir(f"instance/decks/{token}")
+    os.mkdir(f"instance/decks/{token}/anki")
+    file.save(f"instance/decks/{token}/anki/deck.apkg")
+
+    return token
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,6 @@
 from . import app
 import random
 from celery import shared_task
+
+def random_hex_token(length=16):
+    return ''.join(random.choice('0123456789abcdef') for _ in range(length))
