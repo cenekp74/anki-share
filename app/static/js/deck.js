794f8ee2d1ffe250ccbec2e7aa9a6af20565f407
Čeněk Použník <potuznikcenek@gmail.com>
2025-02-22 15:41:02 +0100

    added "correct" and "incorrect" button and functionality

--- app/static/js/deck.js
+++ app/static/js/deck.js
@@ -1,27 +1,44 @@
 function getCurrentCardEle() {
-    return document.getElementById(`card-${window.currentCard}`)
+    return window.cards[window.currentCard]
 }
 
-function changeCurrentCard(card_id) {
+function changeCurrentCard(card_index) {
     prevCard = getCurrentCardEle()
     if (prevCard) {
         prevCard.classList.remove("active")
         prevCard.classList.remove("flipped")
     }
-    window.currentCard = card_id
-    card = document.getElementById(`card-${card_id}`)
+    window.currentCard = card_index
+    card = window.cards[card_index] 
     if (!card) return
     card.classList.add("active")
 }
 
 function nextCard() {
-    if (window.currentCard == document.getElementsByClassName("card").length) return
+    if (window.currentCard == window.cards.length-1) {
+        changeCurrentCard(0)
+        return
+    }
     changeCurrentCard(window.currentCard+1)
 }
 
 function prevCard() {
-    if (window.currentCard == 1) return
+    if (window.currentCard == 0) {
+        changeCurrentCard(window.cards.length-1)
+        return
+    }
     changeCurrentCard(window.currentCard-1)
+}
+
+function correct() {
+    getCurrentCardEle().classList.remove("active")
+    getCurrentCardEle().classList.remove("flipped")
+    window.cards.splice(window.currentCard, 1)
+    changeCurrentCard(window.currentCard)
+}
+
+function incorrect() {
+    nextCard()
 }
 
 function flipCurrentCard() {
@@ -29,10 +46,14 @@ function flipCurrentCard() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    window.cards = []
     document.querySelectorAll(".card").forEach((cardEle) => {
         cardEle.addEventListener("click", flipCurrentCard)
+        window.cards.push(cardEle)
     })
     document.querySelector(".next-button").addEventListener("click", nextCard)
     document.querySelector(".prev-button").addEventListener("click", prevCard)
-    changeCurrentCard(1)
+    document.querySelector(".correct-button").addEventListener("click", correct)
+    document.querySelector(".incorrect-button").addEventListener("click", incorrect)
+    changeCurrentCard(0)
 })
