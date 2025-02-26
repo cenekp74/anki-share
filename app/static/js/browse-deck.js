e66dfe0259fc2708d4dc238d2d6228685fa3647b
Čeněk Použník <potuznikcenek@gmail.com>
2025-02-26 12:47:37 +0100

    typo

--- app/static/js/browse-deck.js
+++ app/static/js/browse-deck.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".card").forEach((cardEle) => {
         cardEle.addEventListener("click", () => {
-            cardIndex = cardEle.id.split("-")[1]
+            cardIndex = cardEle.id.split("-")[1]-1
             document.location.href = document.location.href.replace("/browse", `?card=${cardIndex}`)
         })
     })
